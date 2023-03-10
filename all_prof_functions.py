Fixed some bugs, changed the scaling and normalisation using Mike's psrcelery code, etc.

all_prof_functions.py
@@ -2251,7 +2251,7 @@ def run_each_gp(data, mjds_in, errs=None, kern_len=300, max_num=4, prior_min=200
     mjds = mjds_in - mjds_off
     if mjds_pred is None:
         mjds_pred = np.arange(np.ceil(mjds.max()+1))
-    else:
+    elif mjds_pred.min() > 30000:
         mjds_pred -= mjds_off
         
     # initialise arrays
@@ -2350,6 +2350,11 @@ def run_each_gp(data, mjds_in, errs=None, kern_len=300, max_num=4, prior_min=200
                 if show_plots:
                     plt.show()
 
+        if mjds_pred.min() < mjds.min()+100-mjds_off:
+            mjds_pred += mjds_off
+        elif mjds_pred.max() > mjds.max()-100+mjds_off:
+            mjds_pred -= mjds_off
+            
         pred, pred_var = gp.predict(eigval, mjds_pred, return_var=True)
         pred_res[eignum,:] = pred
         pred_vars[eignum,:] = pred_var
@@ -2369,7 +2374,10 @@ def run_each_gp(data, mjds_in, errs=None, kern_len=300, max_num=4, prior_min=200
                     mjds_off, mjds, data[:,:max_num+1], errs, savename=gp_plotname,
                     bk_bgd=bk_bgd, show=show_plots)
                 
-    return(pred_res, pred_vars, mjds_pred+mjds_off)
+    if mjds_pred.min() < 20000:
+        mjds_pred += mjds_off
+        
+    return(pred_res, pred_vars, mjds_pred)
 
 
 def plot_eig_gp(mjds_pred, pred_res, pred_var, mjd_offset=None,
@@ -2500,8 +2508,10 @@ def plot_eig_gp(mjds_pred, pred_res, pred_var, mjd_offset=None,
                 ax1 = axes_list[0]
                 ax = fig.add_axes((l, b + pan_num*h + pan_num*sep, w, h), sharex=ax1)
                 ax.set_ylabel('$\dot \\nu$ (Hz/s)', fontsize=12)
-                ax.fill_between(nudot_mjds, nudot_vals - np.sqrt(nudot_vars), nudot_vals + np.sqrt(nudot_vars),
-                                color=lc, alpha=k_alpha, zorder=10)
+                if nudot_vars is not None:
+                    ax.fill_between(nudot_mjds, nudot_vals - np.sqrt(nudot_vars), nudot_vals + np.sqrt(nudot_vars),
+                                    color=lc, alpha=k_alpha, zorder=10)
+                    
                 ax.plot(nudot_mjds, nudot_vals, lc, lw=1.5, zorder=20)
                         
         elif num_panels > 1:

first_clean_align.py
@@ -16,10 +16,10 @@ from all_prof_functions import (do_rem_aln, aligndata, smart_align, removebaseli
                                 get_rms_bline, _gauss_2, find_bright, findbrightestprofile,
                                 read_pdv, plot_joydivision, setup_log, read_bad_mjd_file)
 
-try:
-    from all_prof_functions import check_null_prob
-except OSError:
-    print("Cannot load nulling module; skipping that part of the analysis")
+#try:
+#    from all_prof_functions import check_null_prob
+#except OSError:
+#    print("Cannot load nulling module; skipping that part of the analysis")
 
 plt.rcParams["figure.figsize"] = (6, 10)
 
@@ -206,6 +206,17 @@ for psr in psr_list:
                 var_dict[BE+'_mjds_new'] = None
                 var_dict[BE] = True
                 continue
+                
+            if np.any(sorted(var_dict[BE+'_mjds_new']) != var_dict[BE+'_mjds_new']):
+                logger.info("MJDs are not sorted!!")
+                sort_inds = np.argsort(var_dict[BE+'_mjds_new'])
+                var_dict[BE+'_mjds_new'] = var_dict[BE+'_mjds_new'][sort_inds]
+                BE_aligned_new = var_dict[BE+'_aligned'][:,sort_inds]
+                if BE_aligned_new.shape == var_dict[BE+'_aligned'].shape:
+                    var_dict[BE+'_aligned'] = BE_aligned_new
+        
+                if np.any(sorted(var_dict[BE+'_mjds_new']) != var_dict[BE+'_mjds_new']):
+                    logger.warning("Sorting failed!!!!")
                 
             var_dict[BE+'_aligned'] = np.nan_to_num(var_dict[BE+'_aligned'])
 

gp_init_threads.py
@@ -10,7 +10,7 @@ def init_thread(kern_len, _data, mjds, errs=None):
     global data
     data = _data
     variance = np.var(data)
-    term1 = cel.terms.Matern32Term(np.log(1), np.log(kern_len), bounds=dict(log_sigma=(-15, 15), log_rho=(np.log(8.07), np.log(1280.57))))
+    term1 = cel.terms.Matern32Term(np.log(1), np.log(kern_len), bounds=dict(log_sigma=(-15, 15), log_rho=(np.log(51.91), np.log(4279.66))))
     term2 = cel.terms.JitterTerm(np.log(np.sqrt(variance)), bounds=dict(log_sigma=(-15, 15)))
     kernel = term1 + term2
     gp = cel.GP(kernel, np.mean(data), fit_mean=True)
@@ -26,7 +26,7 @@ def lnprob(p):
     global gp
     global data
     # Trivial uniform prior on length scale
-    if p[1] < np.log(8.07) or p[1] > np.log(1280.57):
+    if p[1] < np.log(51.91) or p[1] > np.log(4279.66):
         return(-np.inf)
             
     if np.any((-30 > p) + (p > 30)):